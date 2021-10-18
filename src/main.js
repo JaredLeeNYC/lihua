d611390b749ff17eab96cec24d979920004ebba3
JaredLee <lzbstar@gmail.com>
2021-10-18 12:56:20 +0800

    bugfix

--- src/main.js
+++ src/main.js
@@ -7,14 +7,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  scrollBehavior: function (to, from, savedPosition) {
-    if (to.hash) {
-      return { selector: to.hash };
-      //Or for Vue 3:
-      //return {el: to.hash}
-    } else {
-      return { x: 0, y: 0 };
-    }
-  },
   render: (h) => h(App)
 }).$mount("#app");
